4e562d471c526db1994bf4ed2842c98addbaea66
Ahadinash-Shootorder <ahadinash@shootorder.in>
2026-04-09 18:51:03 +0530

    New layout for home Greta Global Presence or feature-service section

--- news.php
+++ news.php
@@ -1,24 +1,24 @@
-<?php $head_title="Greta Group"?>
+<?php $head_title = "Greta Group" ?>
 <?php require_once('template-parts/layout/top-layout.php'); ?>
 <?php require_once('template-parts/header/header.php'); ?>
 
-<?php 
+<?php
 require_once('template-parts/news/test.php');
 ?>
-<?php 
+<?php
 // require_once('template-parts/news/news-hero.php');
 ?>
-<?php 
+<?php
 // require_once('template-parts/news/news-headline.php');
 ?>
-<?php 
+<?php
 // require_once('template-parts/news/blog-cards.php');
 ?>
-<?php 
+<?php
 // require_once('template-parts/news/news-future-tech.php');
 ?>
-<?php 
-require_once('template-parts/news/news-contact.php');
+<?php
+// require_once('template-parts/news/news-contact.php');
 ?>
 
 
