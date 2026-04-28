f54c1f8b191bb721d78e811f4969bbd75fd89388
Ahadinash-Shootorder <ahadinash@shootorder.in>
2026-04-28 10:20:15 +0530

    remove content form KUSUM METALS

--- template-parts/kusum-metals-pvt-ltd/about.php
+++ template-parts/kusum-metals-pvt-ltd/about.php
@@ -24,28 +24,4 @@
         <!--</div>-->
     </div>
     
-    <div class="industry-page-about-container container">
-    <div class="industry-page-about-content mt-20">
-            <h2 class="industry-page-about-title">GRETA METAL PTE LTD.  </h2>
-            <p class="industry-page-about-description">
-              Greta Metal Pte Ltd, established in 2011 and operating from Singapore, is a key pillar in our group’s global expansion, specializing in general wholesale trade of essential industrial commodities.
-            </p>
-            <p class="industry-page-about-description">Our portfolio focuses on high-demand materials, including:</p>
-            <ul style="list-style-type: disc; padding-left: 20px; margin-top: 10px;">
-    <li style="margin-bottom: 8px; line-height: 1.6;">
-        Semi-finished Steel: Steel Billets and Wire Rods
-    </li>
-    <li style="margin-bottom: 8px; line-height: 1.6;">
-        Ferrous Scrap
-    </li>
-    <li style="margin-bottom: 8px; line-height: 1.6;">
-        Minerals
-    </li>
-</ul>
-            
-     <p class="industry-page-about-description">
-               Our mission is centered on generating value for all stakeholders by catering to specific customer needs and aggressively increasing market share through ethical and efficient global trading. We build lasting worldwide relationships with customers and suppliers through a commitment to integrity, professionalism, and competitive pricing.
-            </p>
-        </div>
-      </div>
 </section>
